9efb3d5fb08095f1bf8e8e1e2acc7138219991b6
VAIII <danilkasuralev@gmail.com>
2025-10-30 10:26:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = 4;
+            double num2 = 5;
+            Console.WriteLine($"Сложение: {Calculator.Add(num1, num2)}");
+            Console.WriteLine($"Вычитание: {Calculator.Subtract(num1, num2)}");
+            Console.WriteLine($"Умножение: {Calculator.Multiply(num1, num2)}");
+            Console.WriteLine($"Деление: {Calculator.Divide(num1, num2)}");
+        }                
+    }
+}
